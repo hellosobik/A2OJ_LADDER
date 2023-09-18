fde493c8637253d49b53e7c623d7ea5145157609
hellosobik <hellosobik@gmail.com>
2023-09-18 12:47:58 +0530

    added a comment on nearly lucky num

--- below_1300/Nearly_Lucky_Number.cpp
+++ below_1300/Nearly_Lucky_Number.cpp
@@ -1,54 +1,39 @@
 #include <iostream>
 #include <string>
-using namespace std;
+using namespace std; // just a cooment
 
-int main(){
-    string num;
-    cin >> num;
-    string res;
-    int exist;
-    for (int i = 0; i < num.size(); i++)
-    {
-        if (num[i]=='7' || num[i]=='4')
-        {
-            exist++;
-        }
-        
+int main() {
+  string num;
+  cin >> num;
+  string res;
+  int exist;
+  for (int i = 0; i < num.size(); i++) {
+    if (num[i] == '7' || num[i] == '4') {
+      exist++;
     }
-    if (exist==0)
-    {
-        cout << "NO";
-        return 0;
-    }
-    
-    
-    for (int i = 0; i < num.size(); i++)
-    {
-        if (num.size()==1)
-        {
-            cout << "NO";
-            return 0;
-        
-        }
-        
-        if (num[i]!='7' && num[i]!='4')
-        {
-            for (int j = i; j < num.size(); j++)
-            {
-                if (num[j]=='7' || num[j]=='4')
-                {
-                    cout << "NO";
-                    return 0;
-                }
-            
-            }
-            cout << "YES";
-            return 0;
-            
-        }
-        
-    }
-    cout << "YES";
+  }
+  if (exist == 0) {
+    cout << "NO";
     return 0;
-    
+  }
+
+  for (int i = 0; i < num.size(); i++) {
+    if (num.size() == 1) {
+      cout << "NO";
+      return 0;
+    }
+
+    if (num[i] != '7' && num[i] != '4') {
+      for (int j = i; j < num.size(); j++) {
+        if (num[j] == '7' || num[j] == '4') {
+          cout << "NO";
+          return 0;
+        }
+      }
+      cout << "YES";
+      return 0;
+    }
+  }
+  cout << "YES";
+  return 0;
 }
